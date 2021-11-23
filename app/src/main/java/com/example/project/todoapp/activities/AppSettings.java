9502d22a31328f79068129206e1123a1b60203f5
Acer <d.m.j-99@abv.bg>
2021-11-23 12:35:03 +0200

    initial commit

---
+++ app/src/main/java/com/example/project/todoapp/activities/AppSettings.java
@@ -0,0 +1,44 @@
+package com.example.project.todoapp.activities;
+
+
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.project.todoapp.R;
+
+public class AppSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle(getString(R.string.settings_title));
+        getPrefFragment();
+    }
+
+    public static class AppPreference extends PreferenceFragment{
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.app_preferences);
+        }
+    }
+
+    //getting the setting fragment
+    private void getPrefFragment(){
+        FragmentManager fragmentManager=getFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+
+
+    }
+}
